170f9897ccf080075dd6332eafa04603710c7f3d
Juan Castillo <jccastillo@gmail.com>
2022-07-01 18:16:43 -0400

    pagina notfound

---
+++ src/modules/shared/pages/NoPageFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h3> Error 404</h3>
+</template>
+
--- src/router/router.js
+++ src/router/router.js
@@ -4,14 +4,15 @@ import { createRouter,createWebHashHistory } from 'vue-router'
 import AboutPage from "../modules/pokemon/pages/AboutPage";
 import Lispages from "../modules/pokemon/pages/ListPage";
 import PokemonPage from "../modules/pokemon/pages/PokemonPage"
+import NotPageFound from "../modules/shared/pages/NoPageFound"
 
 
 const routes = [
     { path: '/', component: Lispages }, 
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NotPageFound },
   ]
-   
 
   const router = createRouter({
     history: createWebHashHistory(),
