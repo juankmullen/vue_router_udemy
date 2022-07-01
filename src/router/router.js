46fad5b3b107bf634727a05cbd3c684adca9ca24
Juan Castillo <jccastillo@gmail.com>
2022-07-01 18:29:45 -0400

    lazyLoad

--- src/router/router.js
+++ src/router/router.js
@@ -1,17 +1,17 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
 
 // rutas
-import AboutPage from "../modules/pokemon/pages/AboutPage";
+//import AboutPage from "../modules/pokemon/pages/AboutPage";
 import Lispages from "../modules/pokemon/pages/ListPage";
 import PokemonPage from "../modules/pokemon/pages/PokemonPage"
 import NotPageFound from "../modules/shared/pages/NoPageFound"
 
 
 const routes = [
-    { path: '/', component: Lispages }, 
-    { path: '/about', component: AboutPage },
-    { path: '/id', component: PokemonPage },
-    { path: '/:pathMatch(.*)*', component: NotPageFound },
+    { path: '/', component: ()=> import(/* webpackChunkName: "ListPage"*/'../modules/pokemon/pages/ListPage') }, 
+    { path: '/about', component: ()=> import(/* webpackChunkName: "AboutPage"*/'../modules/pokemon/pages/AboutPage') },
+    { path: '/id', component: ()=> import(/* webpackChunkName: "PokemonPage"*/'../modules/pokemon/pages/PokemonPage') },
+    { path: '/:pathMatch(.*)*', component: ()=> import(/* webpackChunkName: "NoPageFound"*/'../modules/shared/pages/NoPageFound') },
   ]
 
   const router = createRouter({
